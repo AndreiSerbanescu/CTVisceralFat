e18dc450f7e8fffd23537bab121a439a751e3d9b
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-05-07 18:34:44 +0300

    use common jib lib

--- files/interface/run_container_jip.py
+++ files/interface/run_container_jip.py
@@ -1,71 +1,15 @@
 import os
-import glob
 from listen import visceral_fat_measure_nifti_single
 from common.utils import *
+from common_jip.batch_job import *
 import shutil
-from common.exceptions import TaskFailedException
 
+def handle_output(fat_report_path, element_output_dir):
+    data_share = os.environ["DATA_SHARE_PATH"]
+    full_fat_report_path = os.path.join(data_share, fat_report_path)
 
-def __validate_input_files_and_get_fullpath(files, element_input_dir):
-    if len(files) == 0:
-        log_error(f"No files found in {element_input_dir} - skipping")
-        return "", False
-
-    if files[0].endswith(".nii.gz"):
-        abs_source_file = os.path.join(element_input_dir, files[0])
-        return abs_source_file, True
-
-    elif files[0].endswith(".dcm"):
-        log_error(f"Not able to compute visceral fat measurements for dicom files (only nifti .nii.gz) - skipping")
-        return "", False
-    else:
-        log_error(f"Unrecognised input file type inside {element_input_dir} - skipping")
-        return "", False
-
-
-def  __compute_task(task_method, source_file):
-    try:
-        result, success = task_method(source_file)
-    except Exception as e:
-        raise TaskFailedException(str(e))
-
-    if not success:
-        raise TaskFailedException()
-
-    return result
-
-
-def start_batch_job():
-    setup_logging()
-
-    batch_folders = [f for f in glob.glob(os.path.join('/', os.environ['WORKFLOW_DIR'], os.environ['BATCH_NAME'], '*'))]
-
-    for batch_element_dir in batch_folders:
-
-        element_input_dir = os.path.join(batch_element_dir, os.environ['OPERATOR_IN_DIR'])
-        element_output_dir = os.path.join(batch_element_dir, os.environ['OPERATOR_OUT_DIR'])
-        os.makedirs(element_output_dir, exist_ok=True)
-
-        files = os.listdir(element_input_dir)
-
-        abs_source_file, valid = __validate_input_files_and_get_fullpath(files, element_input_dir)
-
-        if not valid:
-            continue
-
-        try:
-            fat_report_path = __compute_task(visceral_fat_measure_nifti_single, source_file=abs_source_file)
-        except TaskFailedException as e:
-            log_error(f"Task failed with exception {e}")
-            log_error("Skipping")
-            continue
-
-        data_share = os.environ["DATA_SHARE_PATH"]
-        full_fat_report_path = os.path.join(data_share, fat_report_path)
-
-        element_output_name = os.path.join(element_output_dir, "fat_report.txt")
-        shutil.copyfile(full_fat_report_path, element_output_name)
-
+    element_output_name = os.path.join(element_output_dir, "fat_report.txt")
+    shutil.copyfile(full_fat_report_path, element_output_name)
 
 if __name__ == "__main__":
-    start_batch_job()
+    start_batch_job(handle_output_callback=handle_output, task_method=visceral_fat_measure_nifti_single)
